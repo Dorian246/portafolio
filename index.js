140b329bf363c5d4d48011374b2651eeff3eb46e
Dorian De Jesus Zuñiga <106410003+Dorian246@users.noreply.github.com>
2023-02-11 15:39:40 -0600

    commit-atrchivos

---
+++ index.js
@@ -0,0 +1,8 @@
+function eliminar() {
+    var x = document.getElementById("caja");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
